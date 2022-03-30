83bb9538544f9c973943bb54d7de82af08db52c1
LuisMontañoMichel <user@Luis-MacBook-Pro.local>
2022-03-30 23:15:31 +0900

    solved string duplicator

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 
-
-//test writing
+const stringDuplicator = (str, n) => {
+  
+    let newString = ""
+    
+    for(let i = 0; i < n; i++){
+    newString += str
+    }
+    
+    return newString
+  };
